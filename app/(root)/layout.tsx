f100f2483fffe22a547464e24e33f94e8758d7bd
Hirushinirmaliabeyrathne <hirushiabeyrathne@gmail.com>
2025-10-15 12:20:46 +0530

    landingpage-created

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar/>
+      {children}
+      <Footer/>
+    </>
+  );
+}
